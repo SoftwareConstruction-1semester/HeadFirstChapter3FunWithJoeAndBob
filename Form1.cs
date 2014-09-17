1bae77af078de560591d02191504f0f065d0f5bc
Ebbe Vang <ev@evang.dk>
2014-09-17 13:55:08 +0200

    adds project to GIT

---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeadFirstChapter3FunWithJoeAndBob
+{
+    public partial class Form1 : Form
+    {
+        private Guy joe;
+
+        private Guy bob;
+
+        private int bank = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //initialize joe and bob
+            bob = new Guy();
+            bob.Name = "Bob";
+            bob.Cash = 100;
+
+            joe = new Guy();
+            joe.Name = "Joe";
+            joe.Cash = 50;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (bank >= 10)
+            {
+                bank -= joe.ReceiveCash(10);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("The bank is out of money");
+            }
+        }
+
+        public void UpdateForm()
+        {
+            joesCashLabel.Text = joe.Name + " has $" + joe.Cash;
+            bobsCashLabel.Text = bob.Name + " has $" + bob.Cash;
+            bankCashLabel.Text = "The bank has $" + bank;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bank += bob.GiveCash(5);
+            UpdateForm();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // joe gives $10 to bob
+            bob.ReceiveCash(joe.GiveCash(10));
+            UpdateForm();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Bob gives $5 to Joe
+            joe.ReceiveCash(bob.GiveCash(5));
+            UpdateForm();
+        }
+    }
+}
